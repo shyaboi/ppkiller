d01614623b862d9c985365f47765afcadea6f16d
Shyaboi <ian.shane.sears@gmail.com>
2020-09-10 22:03:44 -0700

    successfully killing most adobe process with os system calls to cmd and powershell

--- adobekiller.py
+++ adobekiller.py
@@ -1,13 +1,12 @@
-import os, subprocess
+import os, subprocess, time, sys
 
 # strings with spaces
-def adobeKillSub():
-    import subprocess, sys
-
+def adobeKillSub2():
     subprocess.Popen(["powershell.exe", f'Stop-Process -Name "Adobe Desktop Service"'], stdout=sys.stdout)
     subprocess.Popen(["powershell.exe", f'Stop-Process -Name "AdobeUpdateService"'], stdout=sys.stdout)
-   
+
     print("killing adobe things......")
+
     os.system('taskkill /IM acrotray.exe /F');print("acro tray killed")
     os.system('taskkill /IM CCXProcess.exe /F');print("CCXProcess killed")
     os.system('taskkill /IM AdobeIPCBroker.exe /F');print("AdobeIPCBroker killed")
@@ -23,8 +22,24 @@ def adobeKillSub():
     os.system(f'taskkill /IM CRWindowsClientService.exe /F');print("CRWindowsClientService killed")
     os.system(f'taskkill /IM AdobeCollabSync.exe /F');print("AdobeCollabSync killed")
     os.system(f'taskkill /IM "Adobe CEF Helper.exe" /F');print("CRWindowsClientService killed")
+    os.system(f'taskkill /IM AcrobatNotificationClient.exe /F');print("AcrobatNotificationClient killed")
+    os.system(f'taskkill /IM  Photoshop.exe /F');print("Photoshop killed")
+    os.system(f'taskkill /IM "Adobe Xd.exe" /F');print("xD killed")
+    os.system(f'taskkill /IM Acrobat.exe /F');print("Acrobat killed")
+    os.system(f'taskkill /IM Lightroom.exe /F');print("Lightroom killed")
+    os.system(f'taskkill /IM AdobeNotificationClient.exe /F');print("AdobeNotificationClient killed")
+    os.system(f'taskkill /IM "Creative Cloud Helper.exe" /F');print("Creative Cloud Helper killed")
 
 
-    
     print("Adobe things......Killed")
     return
+
+def adobeKillSub():
+    adobeKillSub2()
+    time.sleep(.2)
+    adobeKillSub2()
+    time.sleep(.2)
+    adobeKillSub2()
+    time.sleep(.2)
+    adobeKillSub2()
+    print("adobe more or less dead")
